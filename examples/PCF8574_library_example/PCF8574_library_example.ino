bd9f6f1de018f7d6fd5e328e5bda157882ba747a
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-14 01:46:51 -0700

    comments reorganized

--- examples/PCF8574_library_example/PCF8574_library_example.ino
+++ examples/PCF8574_library_example/PCF8574_library_example.ino
@@ -1,19 +1,18 @@
 // Copyright Jacob Killelea <jkillelea@protonmail.ch>
 // This example code and the associated libraries are licensed under the GNU GPLV3
 /*
-  For this code to run properly, pins A0, A1, and A2 on the PCF8574 should either be directly
-  grounded or left to float.
-  A LED and associated resistor should be placed
-  in series so that they are directly attached to VCC on one end, and the PCF8574
-  chip on the other.
-  When the PCF8574 pins are "high," they do not allow current to pass through.
-  When the pins are "low," current flows freely to ground and the LEDs light up.
+ * For this code to run properly, pins A0, A1, and A2 on the PCF8574 should either be directly
+ * grounded or left to float.
+ * A LED and associated resistor should be placed in series so that they are
+ * directly attached to VCC on one end, and the PCF8574 chip on the other.
+ * When the PCF8574 pins are "high," they do not allow current to pass through.
+ * When the pins are "low," current flows freely to ground and the LEDs light up.
 */
 #include <Wire.h>
 #include <PCF8574.h>
 
 
-PCF8574 pcf; // create new object from PCF8574.h library, with address 0x38
+PCF8574 pcf; // create new object from PCF8574.h library, with default address 0x38
 
 
 void setup() {
