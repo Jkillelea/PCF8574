97b7ec23e85065b6ad251ccda74ec176d552948f
Jacob Killelea <Jkillelea@protonmail.ch>
2017-05-27 16:29:54 -0600

    Major updates, remove any code for reading, use bitwise math

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -1,84 +1,62 @@
-// Copyright Jacob Killelea <jkillelea@protonmail.ch>
-// controls the PCF8574 I2C GPIO expander chip from TI.
-// code under GNU GPLV3
-
 #include "PCF8574.h"
 
-// PRIVATE
-uint8_t _WRITE_ADDRESS = 0x38; // defaults
-uint8_t _READ_ADDRESS  = 0x39;
-uint8_t pin_array[8]   = {0, 0, 0, 0, 0, 0, 0, 0}; // initial pin state
-uint8_t pin_byte       = 0x00;
-
-
 // PUBLIC
+// CONSTRUCTOR
 PCF8574::PCF8574(int address) {
-  // CONSTRUCTOR
   _WRITE_ADDRESS = address;
   _READ_ADDRESS  = address + 1;
   Wire.begin();
 }
 
 void PCF8574::write(uint8_t pin, uint8_t state) {
-  pin_array[pin] = state;
+  switch (state) {
+    case HIGH:
+      writeHigh(pin);
+      break;
+    case LOW:
+      writeLow(pin);
+      break;
+  }
+}
+
+inline void PCF8574::writeHigh(uint8_t pin) {
+  pin_state |= (1 << pin); // OR that bit with 1 (turn it to 1).
+  writeCallback();         // All the other bits are OR'ed with zero (so not affected)
+}
+
+inline void PCF8574::writeLow(uint8_t pin) {
+  pin_state &= ~(1 << pin); // AND that bit with 0 (turn it to 0)
+  writeCallback();         // All the other bits are AND'ed with 1 (so not affected)
+}
+
+inline void PCF8574::writeAllHigh() {
+  pin_state |= 0xFF;
+  writeCallback();
+}
+
+inline void PCF8574::writeAllLow() {
+  pin_state &= 0x00;
   writeCallback();
 }
 
 void PCF8574::writeAll(uint8_t state) {
-  for (uint8_t i = 0; i < 8; i++) {
-    pin_array[i] = state;
+  switch (state) {
+    case HIGH:
+      writeAllHigh();
+      break;
+    case LOW:
+      writeAllLow();
+      break;
   }
-  writeCallback();
 }
-
-void PCF8574::writeAllHigh() { writeAll(HIGH); }
-void PCF8574::writeAllLow()  { writeAll(LOW); }
-
-
-// uint8_t PCF8574::read(uint8_t pin) {
-//   // TODO => This!
-// }
-
 
 // PRIVATE
 void PCF8574::writeCallback() {
-  //writes pin states, reads things, ect
-  pin_byte = arrayToByte(pin_array);
-  sendInstructionToChip(_WRITE_ADDRESS, pin_byte);
+  sendInstructionToChip(_WRITE_ADDRESS, pin_state);
 }
 
-void PCF8574::readCallback() {
-  //reads things, ect
-  // TODO
-}
-
-void PCF8574::sendInstructionToChip(byte address, byte instruction) {
+void PCF8574::sendInstructionToChip(uint8_t address, uint8_t instruction) {
   Wire.beginTransmission(address);
   Wire.write(instruction);
   Wire.endTransmission();
-}
-
-
-// MATH FUNCTIONS
-int PCF8574::power(int base, int p) {
-  // int-only version of Arduino pow() function
-  if (p == 0) {
-    return 1;
-  }
-  else {
-    int to_return = 1;
-    for (int i = 0; i < p; i++) {
-      to_return *= base;
-    }
-    return to_return;
-  }
-}
-
-uint8_t PCF8574::arrayToByte(uint8_t array[]) {
-  // Takes an array and converts it from binary to its numerical value
-  uint8_t out_byte = 0;
-  for (uint8_t i = 0; i < 8; i++) {
-    out_byte += array[i] * power(2, 7 - i);
-  }
-  return out_byte;
 }
--- PCF8574.h
+++ PCF8574.h
@@ -1,6 +1,3 @@
-// Copyright Jacob Killelea <jkillelea@protonmail.ch>
-// controls the PCF8574 I2C GPIO expander chip from TI.
-// code under GNU GPLV3
 #ifndef PCF8574_H
 #define PCF8574_H
 
@@ -8,26 +5,22 @@
 #include <Wire.h>
 
 class PCF8574 {
-  public:
-    PCF8574(int address = 0x38); // default address, with A0, A1, and A2 all floating or grounded.
-    void write(uint8_t pin, uint8_t state);
-    void writeAll(uint8_t state);
-    void writeAllLow();
-    void writeAllHigh();
+public:
+  PCF8574(int address = 0x38); // default address, with A0, A1, and A2 all floating or grounded.
+  void write(uint8_t pin, uint8_t state);
+  void writeHigh(uint8_t pin);
+  void writeLow(uint8_t pin);
+  void writeAll(uint8_t state);
+  void writeAllLow();
+  void writeAllHigh();
 
-    // uint8_t read(uint8_t pin); // TODO!
+private:
+  uint8_t _WRITE_ADDRESS = 0x38; // defaults
+  uint8_t _READ_ADDRESS  = 0x39;
+  uint8_t pin_state      = 0xFF; // power-on state
 
-  private:
-    uint8_t _WRITE_ADDRESS;
-    uint8_t _READ_ADDRESS;
-    uint8_t pin_array[8];
-    uint8_t pin_byte;
-
-    void readCallback(void);
-    void writeCallback(void);
-    void sendInstructionToChip(byte, byte);
-    int  power(int base, int p);
-    uint8_t arrayToByte(uint8_t array[]);
+  void writeCallback();
+  void sendInstructionToChip(uint8_t address, uint8_t instruction);
 };
 
-#endif /* end of include guard: PCF8574_H*/
+#endif /* end of include guard: PCF8574_H */
