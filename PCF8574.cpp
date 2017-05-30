58d2b926f52b69b735eaea79f11d57e106135fcb
Jacob Killelea <Jkillelea@protonmail.ch>
2017-05-29 21:26:12 -0600

    formatting

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -1,14 +1,15 @@
 #include "PCF8574.h"
 
 // PUBLIC
-// CONSTRUCTOR
 PCF8574::PCF8574(int address) {
+  // CONSTRUCTOR
   _WRITE_ADDRESS = address;
   _READ_ADDRESS  = address + 1;
   Wire.begin();
 }
 
 void PCF8574::write(uint8_t pin, uint8_t state) {
+  // note that digitalWite in Arduino.h takes uint8_t's for HIGH and LOW
   switch (state) {
     case HIGH:
       writeHigh(pin);
@@ -26,20 +27,19 @@ inline void PCF8574::writeHigh(uint8_t pin) {
 
 inline void PCF8574::writeLow(uint8_t pin) {
   pin_state &= ~(1 << pin); // AND that bit with 0 (turn it to 0)
-  writeCallback();         // All the other bits are AND'ed with 1 (so not affected)
+  writeCallback();          // All the other bits are AND'ed with 1 (so not affected)
 }
 
 void PCF8574::writeAll(uint8_t state) {
   switch (state) {
     case HIGH:
       pin_state |= 0xFF;
-      writeCallback();
       break;
     case LOW:
       pin_state &= 0x00;
-      writeCallback();
       break;
   }
+  writeCallback();
 }
 
 // PRIVATE
